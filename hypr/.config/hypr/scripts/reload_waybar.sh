0ce9173c42a1e76aaad6816e051cef495405d0bb
Aaron Gobeyn <aaron.gobeyn@gmail.com>
2025-03-12 21:12:43 +0100

    Update waybar reload according to waybar config change.

--- hypr/.config/hypr/scripts/reload_waybar.sh
+++ hypr/.config/hypr/scripts/reload_waybar.sh
@@ -2,7 +2,9 @@
 
 # Kill the current waybar and refresh it
 killall waybar
-waybar & disown
+waybar --config ~/.config/waybar/top/config.jsonc --style ~/.config/waybar/top/style.css & disown
+waybar --config ~/.config/waybar/bottom/config.jsonc --style ~/.config/waybar/bottom/style.css & disown
+# waybar & disown
 
 # Notification
 notify-send "Reload Waybar"
