dd1aaaf6da487e2bf68cde9b964b41da8f4d7601
Aaron Gobeyn <aaron.gobeyn@gmail.com>
2025-03-12 22:47:30 +0100

    Comment out bottom bar, more a nuisance than a nicety.

--- hypr/.config/hypr/scripts/reload_waybar.sh
+++ hypr/.config/hypr/scripts/reload_waybar.sh
@@ -3,7 +3,7 @@
 # Kill the current waybar and refresh it
 killall waybar
 waybar --config ~/.config/waybar/top/config.jsonc --style ~/.config/waybar/top/style.css & disown
-waybar --config ~/.config/waybar/bottom/config.jsonc --style ~/.config/waybar/bottom/style.css & disown
+# waybar --config ~/.config/waybar/bottom/config.jsonc --style ~/.config/waybar/bottom/style.css & disown
 # waybar & disown
 
 # Notification
